653f4d7e413d6b0b23b044f3e5452af46b8924a9
krzkro4122 <krolkrzysztof1999@gmail.com>
2024-05-18 14:42:53 +0200

    added a CV downloader (i hope_

--- app/Socials.tsx
+++ app/Socials.tsx
@@ -10,6 +10,18 @@ export const Socials = () => {
     <div className="flex justify-end gap-2">
       <Link
         className="shadow-lg border-2 border-black max-h-[7dvh] max-w-[7dvh] lg:max-h-[6dvh] lg:max-w-[6dvh] rounded-md hover:scale-105 hover:bg-gray-100 active:bg-gray-200"
+        href={`{imagePrefix}/cv.png`}
+        download={true}
+      >
+        <Image
+          src={`${imagePrefix}/cv.png`}
+          // src={cvLogo || `${imagePrefix}/cv.png`}
+          alt="Curriculum Vitae"
+          className="p-2 hover:scale-105 active:opacity-80"
+        />
+      </Link>
+      <Link
+        className="shadow-lg border-2 border-black max-h-[7dvh] max-w-[7dvh] lg:max-h-[6dvh] lg:max-w-[6dvh] rounded-md hover:scale-105 hover:bg-gray-100 active:bg-gray-200"
         href="https://github.com/krzkro4122"
         target="_blank"
       >
