181a202d4428cce0d251675260b82f0763dc6152
krzkro4122 <krolkrzysztof1999@gmail.com>
2024-05-18 16:06:24 +0200

    trying a

--- app/Socials.tsx
+++ app/Socials.tsx
@@ -8,13 +8,11 @@ import { publicPrefix } from "./cards";
 export const Socials = () => {
   return (
     <div className="flex justify-end gap-2">
-      <Link
+      <a
         className="shadow-lg border-2 flex items-center justify-center border-black max-h-[7dvh] max-w-[7dvh] lg:max-h-[6dvh] lg:max-w-[6dvh] rounded-md hover:scale-105 hover:bg-gray-100 active:bg-gray-200"
         href={`${publicPrefix}/CV-18-05-2024.pdf`}
-        locale={false}
         target="_blank"
-        download={true}
-        rel="noopener noreferrer"
+        download
       >
         <Image
           src={`${publicPrefix}/cv.png`}
@@ -22,7 +20,7 @@ export const Socials = () => {
           alt="Curriculum Vitae"
           className="p-2 hover:scale-105 active:opacity-80"
         />
-      </Link>
+      </a>
       <Link
         className="shadow-lg border-2 border-black max-h-[7dvh] max-w-[7dvh] lg:max-h-[6dvh] lg:max-w-[6dvh] rounded-md hover:scale-105 hover:bg-gray-100 active:bg-gray-200"
         href="https://github.com/krzkro4122"
--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,7 @@
 const nextConfig = process.env.NODE_ENV === "production" ? {
   output: "export",
   basePath: "/nextjs-github-pages",
-  assetPrefix: "/nextjs-github-pages",
+  assetPrefix: "/",
   images: {
     unoptimized: true,
   },
